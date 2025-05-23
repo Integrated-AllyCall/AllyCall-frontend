8517e479eb4ab5b482492b3237442486f1d34bd7
MS. NATTAWADEE WUTTIVORADIT <pattynp2005@gmail.com>
2025-05-23 19:46:32 +0700

    google map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyDeKa2MzcUTskzB4DOUKR5VQrTD0WAq12c")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,6 @@
+import 'package:allycall/services/location_service.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,11 +10,42 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  GoogleMapController? mapController;
+  LatLng _center = const LatLng(13.7563, 100.5018);
+
+  @override
+  void initState() {
+    super.initState();
+    _initLocation();
+  }
+
+  Future<void> _initLocation() async {
+    try {
+      final position = await getCurrentLocation();
+      final userLatLng = LatLng(position.latitude, position.longitude);
+      setState(() {
+        _center = userLatLng;
+      });
+    } catch (e) {
+      print("Error getting location: $e");
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Text('Map Page'),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.white),
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
+          myLocationEnabled: true,
+        ),
+      ),
     );
   }
 }
